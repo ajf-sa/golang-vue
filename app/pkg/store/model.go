20cf3500f85b01f3609ae4d028e13e40fd335e65
abdulaziz jaiz <abajall@gmail.com>
2021-05-30 16:43:37 +0300

    store model

--- app/pkg/store/model.go
+++ app/pkg/store/model.go
@@ -1,1 +1,13 @@
 package store
+
+import "gorm.io/gorm"
+
+type Store struct {
+	gorm.Model
+	Name       string
+	Descrption string
+	Phone      string
+	Email      string
+	Address    string
+	OwnerID    uint
+}
